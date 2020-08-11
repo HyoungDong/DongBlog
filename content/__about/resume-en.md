57a9c0aac1a53c7459760f412ea376eb0a55aa24
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2020-08-11 18:58:57 +0900

    First commit

---
+++ content/__about/resume-en.md
@@ -0,0 +1,13 @@
+---
+title: 'about'
+date: 2019-1-27 16:21:13
+lang: 'en'
+---
+
+# Hyeoung Dong
+
+<div align="center">
+
+_Thank you for reading my resume._
+
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Dong's Blog`,
+  description: `Blog posted about ...`,
+  author: `[형동]`,
+  introduction: `웹 프로그래밍, 알고리즘, 개발 정리.`,
+  siteUrl: `https://DongDong.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
